731a773b2a7c3e7404bffd78ddddabcf4009c67c
Sreedhara Durgaprasad <dus2001@med.cornell.edu>
2018-06-17 01:39:52 +0000

    Views Split into XML Views and modified i18N texts

---
+++ webapp/controller/S3_JVLines.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("edu.weill.cornell.wbg.fjv.approve.controller.S3_JVLines", {
+		onInit: function() {}
+	});
+});
